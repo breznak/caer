ff5e839cd27210f00526e68848ea030a826d45e3
Luca Longinotti <luca.longinotti@inilabs.com>
2015-09-22 16:24:23 +0000

    cAER: abstract statistics string generation to its own function, so it can be re-used by other modules.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/cAER/trunk@7210 b7f4320f-462c-0410-a916-d9f35bb82d52

--- modules/statistics/statistics.c
+++ modules/statistics/statistics.c
@@ -3,24 +3,33 @@
 #include "base/module.h"
 #include <time.h>
 
-struct statistics_state {
-	struct timespec lastTime;
-	uint64_t totalEventsCounter;
-	uint64_t validEventsCounter;
-};
+#define STAT_STRING "Total events/second: %10" PRIu64 " - Valid events/second: %10" PRIu64
 
-typedef struct statistics_state *statisticsState;
-
+static bool caerStatisticsInit(caerModuleData moduleData);
 static void caerStatisticsRun(caerModuleData moduleData, size_t argsNumber, va_list args);
+static void caerStatisticsExit(caerModuleData moduleData);
 
-static struct caer_module_functions caerStatisticsFunctions = { .moduleInit =
-NULL, .moduleRun = &caerStatisticsRun, .moduleConfig = NULL, .moduleExit = NULL };
+static struct caer_module_functions caerStatisticsFunctions = { .moduleInit = &caerStatisticsInit, .moduleRun =
+	&caerStatisticsRun, .moduleConfig = NULL, .moduleExit = &caerStatisticsExit };
 
 void caerStatistics(uint16_t moduleID, caerEventPacketHeader packetHeader, size_t divisionFactor) {
 	caerModuleData moduleData = caerMainloopFindModule(moduleID, "Statistics");
 
-	caerModuleSM(&caerStatisticsFunctions, moduleData, sizeof(struct statistics_state), 2, packetHeader,
+	caerModuleSM(&caerStatisticsFunctions, moduleData, sizeof(struct caer_statistics_state), 2, packetHeader,
 		divisionFactor);
+}
+
+static bool caerStatisticsInit(caerModuleData moduleData) {
+	caerStatisticsState state = moduleData->moduleState;
+
+	size_t maxStatStringLength = (size_t) snprintf(NULL, 0, STAT_STRING, UINT64_MAX, UINT64_MAX);
+
+	state->currentStatisticsString = calloc(maxStatStringLength + 1, sizeof(char)); // +1 for NUL termination.
+	if (state->currentStatisticsString == NULL) {
+		return (false);
+	}
+
+	return (true);
 }
 
 static void caerStatisticsRun(caerModuleData moduleData, size_t argsNumber, va_list args) {
@@ -30,8 +39,26 @@ static void caerStatisticsRun(caerModuleData moduleData, size_t argsNumber, va_l
 	caerEventPacketHeader packetHeader = va_arg(args, caerEventPacketHeader);
 	size_t divisionFactor = va_arg(args, size_t);
 
-	statisticsState state = moduleData->moduleState;
+	caerStatisticsState state = moduleData->moduleState;
+	state->divisionFactor = divisionFactor;
 
+	caerUpdateStatisticsString(packetHeader, state);
+
+	fprintf(stdout, "\r%s", state->currentStatisticsString);
+	fflush(stdout);
+}
+
+static void caerStatisticsExit(caerModuleData moduleData) {
+	caerStatisticsState state = moduleData->moduleState;
+
+	// Reclaim string memory.
+	if (state->currentStatisticsString != NULL) {
+		free(state->currentStatisticsString);
+		state->currentStatisticsString = NULL;
+	}
+}
+
+void caerUpdateStatisticsString(caerEventPacketHeader packetHeader, caerStatisticsState state) {
 	// Only non-NULL packets (with content!) contribute to the event count.
 	if (packetHeader != NULL) {
 		state->totalEventsCounter += caerEventPacketHeaderGetEventNumber(packetHeader);
@@ -48,13 +75,10 @@ static void caerStatisticsRun(caerModuleData moduleData, size_t argsNumber, va_l
 	// DiffNanoTime is the difference in nanoseconds; we want to trigger roughly every second.
 	if (diffNanoTime >= 1000000000) {
 		// Print current values.
-		uint64_t totalEventsPerTime = (state->totalEventsCounter * (1000000000 / divisionFactor)) / diffNanoTime;
-		uint64_t validEventsPerTime = (state->validEventsCounter * (1000000000 / divisionFactor)) / diffNanoTime;
+		uint64_t totalEventsPerTime = (state->totalEventsCounter * (1000000000 / state->divisionFactor)) / diffNanoTime;
+		uint64_t validEventsPerTime = (state->validEventsCounter * (1000000000 / state->divisionFactor)) / diffNanoTime;
 
-		fprintf(stdout, "\rTotal events/second: %10" PRIu64 " - Valid events/second: %10" PRIu64, totalEventsPerTime,
-			validEventsPerTime);
-
-		fflush(stdout);
+		sprintf(state->currentStatisticsString, STAT_STRING, totalEventsPerTime, validEventsPerTime);
 
 		// Reset for next update.
 		state->totalEventsCounter = 0;
--- modules/statistics/statistics.h
+++ modules/statistics/statistics.h
@@ -4,6 +4,20 @@
 #include "main.h"
 #include "events/common.h"
 
+struct caer_statistics_state {
+	uint64_t divisionFactor;
+	char *currentStatisticsString;
+	// Internal book-keeping.
+	struct timespec lastTime;
+	uint64_t totalEventsCounter;
+	uint64_t validEventsCounter;
+};
+
+typedef struct caer_statistics_state *caerStatisticsState;
+
+// For reuse inside other modules.
+void caerUpdateStatisticsString(caerEventPacketHeader packetHeader, caerStatisticsState state);
+
 void caerStatistics(uint16_t moduleID, caerEventPacketHeader packetHeader, size_t divisionFactor);
 
 #endif /* STATISTICS_H_ */
