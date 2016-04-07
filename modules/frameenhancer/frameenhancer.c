ab0cd2b3aee839e1b639301a8c46436a77ac71f7
Luca Longinotti <luca.longinotti@inilabs.com>
2016-04-07 14:05:37 +0000

    cAER: add frame enhancer module stub.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/cAER/trunk@8509 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ modules/frameenhancer/frameenhancer.c
@@ -0,0 +1,59 @@
+#include "frameenhancer.h"
+#include "base/mainloop.h"
+#include "base/module.h"
+
+struct FrameEnhancer_state {
+
+};
+
+typedef struct FrameEnhancer_state *FrameEnhancerState;
+
+static bool caerFrameEnhancerInit(caerModuleData moduleData);
+static void caerFrameEnhancerRun(caerModuleData moduleData, size_t argsNumber, va_list args);
+static void caerFrameEnhancerConfig(caerModuleData moduleData);
+static void caerFrameEnhancerExit(caerModuleData moduleData);
+
+static struct caer_module_functions caerFrameEnhancerFunctions = { .moduleInit = &caerFrameEnhancerInit, .moduleRun =
+	&caerFrameEnhancerRun, .moduleConfig = &caerFrameEnhancerConfig, .moduleExit = &caerFrameEnhancerExit };
+
+caerFrameEventPacket caerFrameEnhancer(uint16_t moduleID, caerFrameEventPacket frame) {
+	caerModuleData moduleData = caerMainloopFindModule(moduleID, "FrameEnhancer");
+
+	caerModuleSM(&caerFrameEnhancerFunctions, moduleData, sizeof(struct FrameEnhancer_state), 1, frame);
+}
+
+static bool caerFrameEnhancerInit(caerModuleData moduleData) {
+	sshsNodeAddAttributeListener(moduleData->moduleNode, moduleData, &caerModuleConfigDefaultListener);
+
+	FrameEnhancerState state = moduleData->moduleState;
+
+	// Nothing that can fail here.
+	return (true);
+}
+
+static void caerFrameEnhancerRun(caerModuleData moduleData, size_t argsNumber, va_list args) {
+	UNUSED_ARGUMENT(argsNumber);
+
+	// Interpret variable arguments (same as above in main function).
+	caerFrameEventPacket frame = va_arg(args, caerFrameEventPacket);
+
+	// Only process packets with content.
+	if (frame == NULL) {
+		return;
+	}
+
+	FrameEnhancerState state = moduleData->moduleState;
+
+}
+
+static void caerFrameEnhancerConfig(caerModuleData moduleData) {
+	caerModuleConfigUpdateReset(moduleData);
+
+	FrameEnhancerState state = moduleData->moduleState;
+
+}
+
+static void caerFrameEnhancerExit(caerModuleData moduleData) {
+	FrameEnhancerState state = moduleData->moduleState;
+
+}
