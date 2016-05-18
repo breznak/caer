ef7129d4b21f50eb4003ae27930b955433fd53ed
Luca Longinotti <l@longi.li>
2016-05-18 17:04:24 +0200

    Input: skeleton based on output module.

--- modules/misc/in/input_common.c
+++ modules/misc/in/input_common.c
@@ -50,15 +50,199 @@ typedef struct input_common_state *inputCommonState;
 
 size_t CAER_INPUT_COMMON_STATE_STRUCT_SIZE = sizeof(struct input_common_state);
 
+static int inputHandlerThread(void *stateArg);
+static bool newInputBuffer(inputCommonState state);
+static void caerInputCommonConfigListener(sshsNode node, void *userData, enum sshs_node_attribute_events event,
+	const char *changeKey, enum sshs_node_attr_value_type changeType, union sshs_node_attr_value changeValue);
+
+static bool newInputBuffer(inputCommonState state) {
+	// First check if the size really changed.
+	size_t newBufferSize = (size_t) sshsNodeGetInt(state->parentModule->moduleNode, "bufferSize");
+
+	if (state->dataBuffer != NULL && state->dataBuffer->bufferSize == newBufferSize) {
+		// Yeah, we're already where we want to be!
+		return (true);
+	}
+
+	// Allocate new buffer.
+	simpleBuffer newBuffer = malloc(sizeof(*newBuffer) + (newBufferSize * sizeof(uint8_t)));
+	if (newBuffer == NULL) {
+		return (false);
+	}
+
+	// Update new buffer size information.
+	newBuffer->bufferSize = newBufferSize;
+	newBuffer->bufferUsedSize = 0;
+
+	// Commit previous buffer content and then free the memory.
+	if (state->dataBuffer != NULL) {
+		// TODO: handle this.
+
+		free(state->dataBuffer);
+	}
+
+	// Use new buffer.
+	state->dataBuffer = newBuffer;
+
+	return (true);
+}
+
+static int inputHandlerThread(void *stateArg) {
+	inputCommonState state = stateArg;
+
+	while (atomic_load_explicit(&state->running, memory_order_relaxed)) {
+		// Handle configuration changes affecting buffer management.
+		if (atomic_load_explicit(&state->bufferUpdate, memory_order_relaxed)) {
+			atomic_store(&state->bufferUpdate, false);
+
+			if (!newInputBuffer(state)) {
+				caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString,
+					"Failed to allocate new input data buffer. Continue using old one.");
+			}
+		}
+
+		// Signal availability of new data to the mainloop.
+		atomic_fetch_add_explicit(&(caerMainloopGetReference())->dataAvailable, 1, memory_order_release);
+	}
+
+	return (thrd_success);
+}
+
 bool caerInputCommonInit(caerModuleData moduleData, int readFd, bool isNetworkStream,
 bool isNetworkMessageBased) {
+	inputCommonState state = moduleData->moduleState;
+
+	state->parentModule = moduleData;
+
+	// Check for invalid file descriptors.
+	if (readFd < -1) {
+		caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString, "Invalid file descriptor.");
+		return (false);
+	}
+
+	state->fileDescriptor = readFd;
+
+	// Store network/file, message-based or not information.
+	state->isNetworkStream = isNetworkStream;
+	state->isNetworkMessageBased = isNetworkMessageBased;
+
+	// Initial source ID has to be -1 (invalid).
+	state->sourceID = -1;
+
+	// Handle configuration.
+	sshsNodePutBoolIfAbsent(moduleData->moduleNode, "validOnly", false); // only send valid events
+	sshsNodePutBoolIfAbsent(moduleData->moduleNode, "keepPackets", false); // ensure all packets are kept
+	sshsNodePutIntIfAbsent(moduleData->moduleNode, "bufferSize", 65536); // in bytes, size of data buffer
+	sshsNodePutIntIfAbsent(moduleData->moduleNode, "transferBufferSize", 128); // in packet groups
+
+	atomic_store(&state->validOnly, sshsNodeGetBool(moduleData->moduleNode, "validOnly"));
+	atomic_store(&state->keepPackets, sshsNodeGetBool(moduleData->moduleNode, "keepPackets"));
+
+	// Initialize transfer ring-buffer. transferBufferSize only changes here at init time!
+	state->transferRing = ringBufferInit((size_t) sshsNodeGetInt(moduleData->moduleNode, "transferBufferSize"));
+	if (state->transferRing == NULL) {
+		caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString, "Failed to allocate transfer ring-buffer.");
+		return (false);
+	}
+
+	// Allocate data buffer. bufferSize is updated here.
+	if (!newInputBuffer(state)) {
+		ringBufferFree(state->transferRing);
+
+		caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString, "Failed to allocate input data buffer.");
+		return (false);
+	}
+
+	// Start input handling thread.
+	atomic_store(&state->running, true);
+
+	if (thrd_create(&state->inputThread, &inputHandlerThread, state) != thrd_success) {
+		ringBufferFree(state->transferRing);
+		free(state->dataBuffer);
+
+		caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString, "Failed to start input handling thread.");
+		return (false);
+	}
+
+	// Add config listeners last, to avoid having them dangling if Init doesn't succeed.
+	sshsNodeAddAttributeListener(moduleData->moduleNode, moduleData, &caerInputCommonConfigListener);
+
 	return (true);
 }
 
 void caerInputCommonExit(caerModuleData moduleData) {
+	// Remove listener, which can reference invalid memory in userData.
+	sshsNodeRemoveAttributeListener(moduleData->moduleNode, moduleData, &caerInputCommonConfigListener);
 
+	inputCommonState state = moduleData->moduleState;
+
+	// Stop input thread and wait on it.
+	atomic_store(&state->running, false);
+
+	if ((errno = thrd_join(state->inputThread, NULL)) != thrd_success) {
+		// This should never happen!
+		caerLog(CAER_LOG_CRITICAL, state->parentModule->moduleSubSystemString,
+			"Failed to join input handling thread. Error: %d.", errno);
+	}
+
+	// Now clean up the transfer ring-buffer and its contents.
+	caerEventPacketContainer packetContainer;
+	while ((packetContainer = ringBufferGet(state->transferRing)) != NULL) {
+		caerEventPacketContainerFree(packetContainer);
+
+		atomic_fetch_sub_explicit(&(caerMainloopGetReference())->dataAvailable, 1, memory_order_relaxed);
+	}
+
+	ringBufferFree(state->transferRing);
+
+	// Close file descriptors.
+	if (state->fileDescriptor >= 0) {
+		close(state->fileDescriptor);
+	}
+
+	// Free allocated memory.
+	free(state->dataBuffer);
 }
 
 void caerInputCommonRun(caerModuleData moduleData, size_t argsNumber, va_list args) {
+	UNUSED_ARGUMENT(argsNumber);
 
+	inputCommonState state = moduleData->moduleState;
+
+	// Interpret variable arguments (same as above in main function).
+	caerEventPacketContainer *container = va_arg(args, caerEventPacketContainer *);
+
+	*container = ringBufferGet(state->transferRing);
+
+	if (*container != NULL) {
+		// Got a container, set it up for auto-reclaim and signal it's not available anymore.
+		caerMainloopFreeAfterLoop((void (*)(void *)) &caerEventPacketContainerFree, *container);
+
+		// No special memory order for decrease, because the acquire load to even start running
+		// through a mainloop already synchronizes with the release store above.
+		atomic_fetch_sub_explicit(&(caerMainloopGetReference())->dataAvailable, 1, memory_order_relaxed);
+	}
+}
+
+static void caerInputCommonConfigListener(sshsNode node, void *userData, enum sshs_node_attribute_events event,
+	const char *changeKey, enum sshs_node_attr_value_type changeType, union sshs_node_attr_value changeValue) {
+	UNUSED_ARGUMENT(node);
+
+	caerModuleData moduleData = userData;
+	inputCommonState state = moduleData->moduleState;
+
+	if (event == ATTRIBUTE_MODIFIED) {
+		if (changeType == BOOL && caerStrEquals(changeKey, "validOnly")) {
+			// Set valid only flag to given value.
+			atomic_store(&state->validOnly, changeValue.boolean);
+		}
+		else if (changeType == BOOL && caerStrEquals(changeKey, "keepPackets")) {
+			// Set keep packets flag to given value.
+			atomic_store(&state->keepPackets, changeValue.boolean);
+		}
+		else if (changeType == INT && caerStrEquals(changeKey, "bufferSize")) {
+			// Set buffer update flag.
+			atomic_store(&state->bufferUpdate, true);
+		}
+	}
 }
