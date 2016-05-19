9b0ec8f19f6016c3ab8e90fe62926644117e0fec
Luca Longinotti <l@longi.li>
2016-05-19 10:31:35 +0200

    Ext/Buffers: add simpleBufferInit() to handle memory allocation and initialization. Add concept of buffer position, an internal offset.

--- ext/buffers.h
+++ ext/buffers.h
@@ -7,6 +7,8 @@
 #include "nets.h"
 
 struct simple_buffer {
+	/// Current position inside buffer.
+	size_t bufferPosition;
 	/// Size of data currently inside buffer, in bytes.
 	size_t bufferUsedSize;
 	/// Size of buffer, in bytes.
@@ -17,20 +19,41 @@ struct simple_buffer {
 
 typedef struct simple_buffer *simpleBuffer;
 
+static inline simpleBuffer simpleBufferInit(size_t size) {
+	// Allocate new buffer.
+	simpleBuffer newBuffer = malloc(sizeof(*newBuffer) + (size * sizeof(uint8_t)));
+	if (newBuffer == NULL) {
+		return (NULL);
+	}
+
+	// Update new buffer size information.
+	newBuffer->bufferSize = size;
+	newBuffer->bufferUsedSize = 0;
+	newBuffer->bufferPosition = 0;
+
+	return (newBuffer);
+}
+
 static inline bool simpleBufferWrite(int fd, simpleBuffer buffer) {
-	return (writeUntilDone(fd, buffer->buffer, buffer->bufferUsedSize));
+	if (buffer->bufferPosition > buffer->bufferUsedSize) {
+		// Position is after any valid data, this can't work!
+		return (false);
+	}
+
+	return (writeUntilDone(fd, buffer->buffer + buffer->bufferPosition, buffer->bufferUsedSize - buffer->bufferPosition));
 }
 
 static inline bool simpleBufferRead(int fd, simpleBuffer buffer) {
 	// Try to fill whole buffer.
-	ssize_t result = readUntilDone(fd, buffer->buffer, buffer->bufferSize);
+	ssize_t result = readUntilDone(fd, buffer->buffer + buffer->bufferPosition,
+		buffer->bufferSize - buffer->bufferPosition);
 	if (result <= 0) {
 		// Error or EOF with no data.
 		return (false);
 	}
 
 	// Actual data, update UsedSize.
-	buffer->bufferUsedSize = (size_t) result;
+	buffer->bufferUsedSize = buffer->bufferPosition + (size_t) result;
 	return (true);
 }
 
