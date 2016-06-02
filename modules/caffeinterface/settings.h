1027824405a0d7ccce1469ad692c4e909fff2f90
federico <federico.corradi@gmail.com>
2016-06-02 11:49:35 +0200

    retrained network for face detection

--- modules/caffeinterface/settings.h
+++ modules/caffeinterface/settings.h
@@ -20,6 +20,6 @@
 //#define NET_VAL "/home/federico/space/inilabs/3rdparty/caffe/examples/faces_36x36/val.txt";
 // DVS FACE DETECTION - remember to enable save_png and set detTreshold   
 #define NET_MODEL "modules/caffeinterface/caffe_models/faces_36x36/lenet.prototxt";
-#define NET_WEIGHTS  "modules/caffeinterface/caffe_models/faces_36x36/_iter_510000.caffemodel";
+#define NET_WEIGHTS  "modules/caffeinterface/caffe_models/faces_36x36/binary.caffemodel";
 #define NET_MEAN "modules/caffeinterface/caffe_models/faces_36x36/mean.mean";
 #define NET_VAL "modules/caffeinterface/caffe_models/faces_36x36/val.txt";
