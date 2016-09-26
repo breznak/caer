152406cd26baabdba9e9b27172f59d071da22b19
Luca Longinotti <l@longi.li>
2016-09-26 14:52:04 +0200

    Input: more informative error message.

--- modules/misc/in/input_common.c
+++ modules/misc/in/input_common.c
@@ -1400,7 +1400,8 @@ static int inputReaderThread(void *stateArg) {
 		// Parse header and setup header info structure.
 		if (!state->header.isValidHeader && !parseHeader(state)) {
 			// Header invalid, exit.
-			caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString, "Failed to parse header.");
+			caerLog(CAER_LOG_ERROR, state->parentModule->moduleSubSystemString,
+				"Failed to parse header. Only AEDAT 2.X and 3.x compliant files are supported.");
 			atomic_store(&state->inputReaderThreadState, ERROR_HEADER); // Error in Header
 			break;
 		}
