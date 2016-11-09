55f43a7777075dee47d283299a709a6ed310178d
federicohyo <federico.corradi@gmail.com>
2016-11-10 00:31:44 +0100

    finally sending default bias configuration using sshNode and uthash

--- modules/ini/dynapse_common.c
+++ modules/ini/dynapse_common.c
@@ -1,6 +1,7 @@
 #include "dynapse_common.h"
 
-static uint32_t convertBias(struct caer_bias_dynapse coarseFineBias, const char *biasName);
+static uint32_t convertBias(const char *biasName, const char* lowhi,
+                            const char*cl, const char*sex, uint8_t enal,  uint8_t fineValue, uint8_t coarseValue, uint8_t special);
 static uint32_t generateCoarseFineBiasParent(sshsNode biasNode, const char *biasName);
 
 static void biasConfigListener(sshsNode node, void *userData,
@@ -39,26 +40,6 @@ static void moduleShutdownNotify(void *p) {
 
 	// Ensure parent also shuts down (on disconnected device for example).
 	sshsNodePutBool(moduleNode, "running", false);
-}
-
-static void createSpecialBiasSetting(sshsNode biasNode, const char *biasName,
-		uint8_t coarseValue, uint8_t fineValue) {
-
-	// Add trailing slash to node name (required!).
-	size_t biasNameLength = strlen(biasName);
-	char biasNameFull[biasNameLength + 2];
-	memcpy(biasNameFull, biasName, biasNameLength);
-	biasNameFull[biasNameLength] = '/';
-	biasNameFull[biasNameLength + 1] = '\0';
-
-	// Create configuration node for this particular bias.
-	sshsNode biasConfigNode = sshsGetRelativeNode(biasNode, biasNameFull);
-
-	// Add bias settings.
-	sshsNodePutShortIfAbsent(biasConfigNode, "coarseValue", I8T(coarseValue));
-	sshsNodePutShortIfAbsent(biasConfigNode, "fineValue", I16T(fineValue));
-	sshsNodePutBoolIfAbsent(biasConfigNode, "special", true);
-
 }
 
 static void createCoarseFineBiasSetting(sshsNode biasNode, const char *biasName,
@@ -112,7 +93,7 @@ static void createDefaultConfiguration(caerModuleData moduleData,
 	sshsNode deviceConfigNode = sshsGetRelativeNode(moduleData->moduleNode,
 			chipIDToName(devInfo->chipID, true));
 
-// Chip biases, based on testing defaults.
+// Chip biases, defaults.
 	sshsNode biasNode = sshsGetRelativeNode(deviceConfigNode, "bias/");
 
 	createCoarseFineBiasSetting(biasNode, "C0_IF_BUF_P", 7, 0, "HighBias",
@@ -317,56 +298,24 @@ static void createDefaultConfiguration(caerModuleData moduleData,
 			"Normal", "PBias", true);
 
 	/*special biases*/
-	createSpecialBiasSetting(biasNode, "U_Buffer", 1, 80);
-	createSpecialBiasSetting(biasNode, "U_SSP", 0, 7);
-	createSpecialBiasSetting(biasNode, "U_SSN", 0, 15);
+    createCoarseFineBiasSetting(biasNode, "U_Buffer", 1, 80, "HighBias",
+                             "Normal", "PBias", true);
+    createCoarseFineBiasSetting(biasNode, "U_SSP", 0, 7,"HighBias",
+                             "Normal", "PBias", true);
+    createCoarseFineBiasSetting(biasNode, "U_SSN", 0, 15,"HighBias",
+                             "Normal", "PBias", true);
 
-	createSpecialBiasSetting(biasNode, "D_Buffer", 1, 80);
-	createSpecialBiasSetting(biasNode, "D_SSP", 0, 7);
-	createSpecialBiasSetting(biasNode, "D_SSN", 0, 15);
+    createCoarseFineBiasSetting(biasNode, "D_Buffer", 1, 80, "HighBias",
+                             "Normal", "PBias", true);
+    createCoarseFineBiasSetting(biasNode, "D_SSP", 0, 7, "HighBias",
+                             "Normal", "PBias", true);
+    createCoarseFineBiasSetting(biasNode, "D_SSN", 0, 15, "HighBias",
+                             "Normal", "PBias", true);
 
 }
 
-static uint32_t generateCoarseFineBias(sshsNode biasNode, const char *biasName) {
-// Build up bias value from all its components.
-	char *BiasLowHiStr = sshsNodeGetString(biasNode, "BiasLowHi");
-	char *currentLevelStr = sshsNodeGetString(biasNode, "currentLevel");
-	char *sexStr = sshsNodeGetString(biasNode, "sex");
-
-	struct caer_bias_dynapse biasValue = { .coarseValue = U8T(
-			sshsNodeGetShort(biasNode, "coarseValue")), .fineValue = U8T(
-			sshsNodeGetShort(biasNode, "fineValue")), .BiasLowHi =
-			caerStrEquals(BiasLowHiStr, "HighBias"), .currentLevel =
-			caerStrEquals(currentLevelStr, "Normal"), .sex = caerStrEquals(
-			sexStr, "NBias"), .enabled = U8T(
-			sshsNodeGetBool(biasNode, "enabled")), };
-
-// Free strings to avoid memory leaks.
-	free(BiasLowHiStr);
-	free(currentLevelStr);
-	free(sexStr);
-
-	return (convertBias(biasValue,biasName));
-}
-
-static uint32_t generateCoarseFineBiasParent(sshsNode biasNode, const char *biasName) {
-// Add trailing slash to node name (required!).
-	size_t biasNameLength = strlen(biasName);
-	char biasNameFull[biasNameLength + 2];
-	memcpy(biasNameFull, biasName, biasNameLength);
-	biasNameFull[biasNameLength] = '/';
-	biasNameFull[biasNameLength + 1] = '\0';
-
-	// Get bias configuration node.
-	sshsNode biasConfigNode = sshsGetRelativeNode(biasNode, biasNameFull);
-
-	/*Here always get wrong node.... */
-	caerLog(CAER_LOG_CRITICAL, "BIAS CONFIGURE ", " biasNameFull %s \n", biasNameFull);
-
-	return (generateCoarseFineBias(biasConfigNode, biasNameFull));
-}
-
-static uint32_t convertBias(struct caer_bias_dynapse coarseFineBias, const char *biasName){
+static uint32_t convertBias(const char *biasName, const char* lowhi,
+                            const char*cl, const char*sex, uint8_t enal,  uint8_t fineValue, uint8_t coarseValue, uint8_t special){
 
 		int32_t confbits;
 		int32_t addr = 0;
@@ -698,25 +647,44 @@ static uint32_t convertBias(struct caer_bias_dynapse coarseFineBias, const char 
 			addr = DYNAPSE_CONFIG_BIAS_D_SSN;
 		}
 
-		caerLog(CAER_LOG_CRITICAL, "BIAS CONFIGURE ", " biasName %s --> ADDR %d\n", biasName, addr);
+    
+        uint8_t lws, ssx, cls;
+        if(caerStrEquals(lowhi, "HighBias")){
+            lws = 1;
+        }else{
+            lws = 0;
+        }
+        if(caerStrEquals(sex, "NBias")){
+            ssx = 1;
+        }else{
+            ssx = 0;
+        }
+        if(caerStrEquals(sex, "Normal")){
+            cls = 1;
+        }else{
+            cls = 0;
+        }
+        
+    
+		caerLog(CAER_LOG_DEBUG, "BIAS CONFIGURE ", " biasName %s --> ADDR %d\n", biasName, addr);
 
 		/*end names*/
 
-		confbits = coarseFineBias.BiasLowHi << 3 | coarseFineBias.currentLevel << 2 | coarseFineBias.sex << 1 | coarseFineBias.enabled;
+		confbits = lws << 3 | cls << 2 | ssx << 1 | enal;
 
 		// snn and ssp
 		if(addr == 51 || addr == 52 || addr == 115 || addr == 116){
-			inbits = addr << 18 | 1 << 16 | 63 << 10 | coarseFineBias.fineValue << 4 | confbits;
+			inbits = addr << 18 | 1 << 16 | 63 << 10 | fineValue << 4 | confbits;
 		}else{
-			inbits = addr << 18 | 1 << 16 | coarseFineBias.special << 15 | coarseFineBias.coarseValue << 12 | coarseFineBias.fineValue << 4 | confbits;
+			inbits = addr << 18 | 1 << 16 | special << 15 | coarseValue << 12 | fineValue << 4 | confbits;
 		}
 
-		caerLog(CAER_LOG_CRITICAL, "BIAS CONFIGURE ", "coarseFineBias.fineValue %d , "
+		caerLog(CAER_LOG_DEBUG, "BIAS CONFIGURE ", "coarseFineBias.fineValue %d , "
 									"coarseFineBias.currentLevel %d, "
 									"coarseFineBias.coarseValue %d ,  "
 									"coarseFineBias.special %d --> ADDR %d\n",
-									coarseFineBias.fineValue, coarseFineBias.currentLevel,
-									coarseFineBias.coarseValue,  coarseFineBias.special, addr);
+									fineValue, cls,
+									coarseValue, special, addr);
 
 		return inbits;
 
@@ -725,149 +693,70 @@ static uint32_t convertBias(struct caer_bias_dynapse coarseFineBias, const char 
 static void biasConfigSend(sshsNode node, caerModuleData moduleData,
 		struct caer_dynapse_info *devInfo) {
 
-	uint32_t value;
 
   //  10100010111000000001101
   //1110100011111110000001101
+    
+    // get the number of childrens biases
+	uint32_t value;
+    size_t biasNodesLength = 0;
+    sshsNode *biasNodes = sshsNodeGetChildren(node, &biasNodesLength);
+    const char *nodeName = sshsNodeGetName(node);
+    caerLog(CAER_LOG_DEBUG, moduleData->moduleSubSystemString, "BIAS LENGHT ... %d NAME %s\n",
+            biasNodesLength, nodeName);
+    
+    // SEND DEFAULT BIASES TO ALL CHIPS in BOARD (0,3) only chip id 4 for now
+    for(uint8_t this_chip = 4; this_chip <= 4; this_chip++){
+        // Let's select this chip for configuration
+        caerDeviceConfigSet(moduleData->moduleState, DYNAPSE_CONFIG_CHIP, DYNAPSE_CONFIG_CHIP_ID,
+                            this_chip);
+        
+        // send configuration, one bias per time
+        if (biasNodes != NULL) {
+            for (size_t i = 0; i < biasNodesLength; i++) {
+                //sshsNodeRemoveAttributeListener(biasNodes[i], moduleData, &biasConfigListener);
+                const char *nodeName = sshsNodeGetName(biasNodes[i]);
+                
+                //value = generateCoarseFineBiasParent(biasNodes[i], nodeName);
+                bool enal = sshsNodeGetBool(biasNodes[i], "enabled");
+                bool special = sshsNodeGetBool(biasNodes[i], "special");
+                uint8_t coarseValue = sshsNodeGetShort(biasNodes[i], "coarseValue");
+                uint8_t fineValue = sshsNodeGetShort(biasNodes[i], "fineValue");
+                const char * lowhi = sshsNodeGetString(biasNodes[i], "BiasLowHi");
+                const char * cl = sshsNodeGetString(biasNodes[i], "currentLevel");
+                const char * sex = sshsNodeGetString(biasNodes[i], "sex");
 
-
-
-	value = generateCoarseFineBiasParent(node, "C0_IF_BUF_P");
-	caerLog(CAER_LOG_CRITICAL, moduleData->moduleSubSystemString, "BIAS C0_IF_BUF_P VALUE ... %d\n", value);
-	caerDeviceConfigSet(moduleData->moduleState, DYNAPSE_CONFIG_CHIP, DYNAPSE_CONFIG_CHIP_CONTENT, value);
-
-	value = generateCoarseFineBiasParent(node, "C0_IF_RFR_N");
-	caerLog(CAER_LOG_CRITICAL, moduleData->moduleSubSystemString, "BIAS C0_IF_RFR_N VALUE ... %d\n", value);
-	caerDeviceConfigSet(moduleData->moduleState, DYNAPSE_CONFIG_CHIP, DYNAPSE_CONFIG_CHIP_CONTENT, value);
-
-/*#define DYNAPSE_CONFIG_BIAS_C0_PULSE_PWLK_P             	0
-#define DYNAPSE_CONFIG_BIAS_C0_PS_WEIGHT_INH_S_N            2
-#define DYNAPSE_CONFIG_BIAS_C0_PS_WEIGHT_INH_F_N            4
-#define DYNAPSE_CONFIG_BIAS_C0_PS_WEIGHT_EXC_S_N         	6
-#define DYNAPSE_CONFIG_BIAS_C0_PS_WEIGHT_EXC_F_N        	8
-#define DYNAPSE_CONFIG_BIAS_C0_IF_RFR_N          			10
-#define DYNAPSE_CONFIG_BIAS_C0_IF_TAU1_N         			12
-#define DYNAPSE_CONFIG_BIAS_C0_IF_AHTAU_N           		14
-#define DYNAPSE_CONFIG_BIAS_C0_IF_CASC_N 					16
-#define DYNAPSE_CONFIG_BIAS_C0_IF_TAU2_N         			18
-#define DYNAPSE_CONFIG_BIAS_C0_IF_BUF_P               		20
-#define DYNAPSE_CONFIG_BIAS_C0_IF_AHTHR_N             		22
-#define DYNAPSE_CONFIG_BIAS_C0_IF_THR_N            			24
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPIE_THR_S_P             	26
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPIE_THR_F_P            	38
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPII_THR_F_P      			30
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPII_THR_S_P            	32
-#define DYNAPSE_CONFIG_BIAS_C0_IF_NMDA_N            		34
-#define DYNAPSE_CONFIG_BIAS_C0_IF_DC_P           			36
-#define DYNAPSE_CONFIG_BIAS_C0_IF_AHW_P          			38
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPII_TAU_S_P          		40
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPII_TAU_F_P 				42
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPIE_TAU_F_P         		44
-#define DYNAPSE_CONFIG_BIAS_C0_NPDPIE_TAU_S_P               46
-#define DYNAPSE_CONFIG_BIAS_C0_R2R_P               			48
-
-#define DYNAPSE_CONFIG_BIAS_C1_PULSE_PWLK_P             	1
-#define DYNAPSE_CONFIG_BIAS_C1_PS_WEIGHT_INH_S_N            3
-#define DYNAPSE_CONFIG_BIAS_C1_PS_WEIGHT_INH_F_N            5
-#define DYNAPSE_CONFIG_BIAS_C1_PS_WEIGHT_EXC_S_N         	7
-#define DYNAPSE_CONFIG_BIAS_C1_PS_WEIGHT_EXC_F_N        	9
-#define DYNAPSE_CONFIG_BIAS_C1_IF_RFR_N          			11
-#define DYNAPSE_CONFIG_BIAS_C1_IF_TAU1_N         			13
-#define DYNAPSE_CONFIG_BIAS_C1_IF_AHTAU_N           		15
-#define DYNAPSE_CONFIG_BIAS_C1_IF_CASC_N 					17
-#define DYNAPSE_CONFIG_BIAS_C1_IF_TAU2_N         			19
-#define DYNAPSE_CONFIG_BIAS_C1_IF_BUF_P               		21
-#define DYNAPSE_CONFIG_BIAS_C1_IF_AHTHR_N             		23
-#define DYNAPSE_CONFIG_BIAS_C1_IF_THR_N            			25
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPIE_THR_S_P             	27
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPIE_THR_F_P            	29
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPII_THR_F_P      			31
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPII_THR_S_P            	33
-#define DYNAPSE_CONFIG_BIAS_C1_IF_NMDA_N            		35
-#define DYNAPSE_CONFIG_BIAS_C1_IF_DC_P           			37
-#define DYNAPSE_CONFIG_BIAS_C1_IF_AHW_P          			39
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPII_TAU_S_P          		41
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPII_TAU_F_P 				43
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPIE_TAU_F_P         		45
-#define DYNAPSE_CONFIG_BIAS_C1_NPDPIE_TAU_S_P               47
-#define DYNAPSE_CONFIG_BIAS_C1_R2R_P               			49
-
-#define DYNAPSE_CONFIG_BIAS_U_BUFFER         				50
-#define DYNAPSE_CONFIG_BIAS_U_SSP               			51
-#define DYNAPSE_CONFIG_BIAS_U_SSN               			52
-
-#define DYNAPSE_CONFIG_BIAS_C2_PULSE_PWLK_P             	64
-#define DYNAPSE_CONFIG_BIAS_C2_PS_WEIGHT_INH_S_N            66
-#define DYNAPSE_CONFIG_BIAS_C2_PS_WEIGHT_INH_F_N            68
-#define DYNAPSE_CONFIG_BIAS_C2_PS_WEIGHT_EXC_S_N         	70
-#define DYNAPSE_CONFIG_BIAS_C2_PS_WEIGHT_EXC_F_N        	72
-#define DYNAPSE_CONFIG_BIAS_C2_IF_RFR_N          			74
-#define DYNAPSE_CONFIG_BIAS_C2_IF_TAU1_N         			76
-#define DYNAPSE_CONFIG_BIAS_C2_IF_AHTAU_N           		78
-#define DYNAPSE_CONFIG_BIAS_C2_IF_CASC_N 					80
-#define DYNAPSE_CONFIG_BIAS_C2_IF_TAU2_N         			82
-#define DYNAPSE_CONFIG_BIAS_C2_IF_BUF_P               		84
-#define DYNAPSE_CONFIG_BIAS_C2_IF_AHTHR_N             		86
-#define DYNAPSE_CONFIG_BIAS_C2_IF_THR_N            			88
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPIE_THR_S_P             	90
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPIE_THR_F_P            	92
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPII_THR_F_P      			94
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPII_THR_S_P            	96
-#define DYNAPSE_CONFIG_BIAS_C2_IF_NMDA_N            		98
-#define DYNAPSE_CONFIG_BIAS_C2_IF_DC_P           			100
-#define DYNAPSE_CONFIG_BIAS_C2_IF_AHW_P          			102
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPII_TAU_S_P          		104
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPII_TAU_F_P 				106
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPIE_TAU_F_P         		108
-#define DYNAPSE_CONFIG_BIAS_C2_NPDPIE_TAU_S_P               110
-#define DYNAPSE_CONFIG_BIAS_C2_R2R_P               			112
-
-#define DYNAPSE_CONFIG_BIAS_C3_PULSE_PWLK_P             	65
-#define DYNAPSE_CONFIG_BIAS_C3_PS_WEIGHT_INH_S_N            67
-#define DYNAPSE_CONFIG_BIAS_C3_PS_WEIGHT_INH_F_N            69
-#define DYNAPSE_CONFIG_BIAS_C3_PS_WEIGHT_EXC_S_N         	71
-#define DYNAPSE_CONFIG_BIAS_C3_PS_WEIGHT_EXC_F_N        	73
-#define DYNAPSE_CONFIG_BIAS_C3_IF_RFR_N          			75
-#define DYNAPSE_CONFIG_BIAS_C3_IF_TAU1_N         			77
-#define DYNAPSE_CONFIG_BIAS_C3_IF_AHTAU_N           		79
-#define DYNAPSE_CONFIG_BIAS_C3_IF_CASC_N 					81
-#define DYNAPSE_CONFIG_BIAS_C3_IF_TAU2_N         			83
-#define DYNAPSE_CONFIG_BIAS_C3_IF_BUF_P               		85
-#define DYNAPSE_CONFIG_BIAS_C3_IF_AHTHR_N             		87
-#define DYNAPSE_CONFIG_BIAS_C3_IF_THR_N            			89
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPIE_THR_S_P             	91
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPIE_THR_F_P            	93
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPII_THR_F_P      			95
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPII_THR_S_P            	97
-#define DYNAPSE_CONFIG_BIAS_C3_IF_NMDA_N            		99
-#define DYNAPSE_CONFIG_BIAS_C3_IF_DC_P           			101
-#define DYNAPSE_CONFIG_BIAS_C3_IF_AHW_P          			103
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPII_TAU_S_P          		105
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPII_TAU_F_P 				107
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPIE_TAU_F_P         		109
-#define DYNAPSE_CONFIG_BIAS_C3_NPDPIE_TAU_S_P               111
-#define DYNAPSE_CONFIG_BIAS_C3_R2R_P               			113
-
-#define DYNAPSE_CONFIG_BIAS_D_BUFFER         				114
-#define DYNAPSE_CONFIG_BIAS_D_SSP               			115
-#define DYNAPSE_CONFIG_BIAS_D_SSN               			116*/
-
-
-
+                // generates bits values
+                uint32_t bits = convertBias(nodeName, lowhi, cl, sex, enal, fineValue, coarseValue, special);
+                
+                caerLog(CAER_LOG_DEBUG, moduleData->moduleSubSystemString, "BIAS LENGHT ... %d NAME %s enabled %d special %d coarseValue %d bits %d\n",
+                        biasNodesLength, nodeName, enal, special, coarseValue, bits);
+             
+                // finally send configuration via USB
+                caerDeviceConfigSet(moduleData->moduleState, DYNAPSE_CONFIG_CHIP,
+                                    DYNAPSE_CONFIG_CHIP_CONTENT, bits);
+                
+                //free strings
+                free(lowhi);
+                free(cl);
+                free(sex);
+                
+            }
+            free(biasNodes);
+        }
+        
+    }
 
 }
-
 
 static void sendDefaultConfiguration(caerModuleData moduleData,
 		struct caer_dynapse_info *devInfo) {
 	// Device related configuration has its own sub-node.
 	sshsNode deviceConfigNode = sshsGetRelativeNode(moduleData->moduleNode,
 			chipIDToName(devInfo->chipID, true));
-
+    
 	// send default bias configuration
 	biasConfigSend(sshsGetRelativeNode(deviceConfigNode, "bias/"), moduleData, devInfo);
-
-
 
 }
 
@@ -889,7 +778,7 @@ bool caerInputDYNAPSEInit(caerModuleData moduleData, uint16_t deviceType) {
 // shutdown cases (device pulled, ...).
 	char *serialNumber = sshsNodeGetString(moduleData->moduleNode,
 			"serialNumber");
-	caerLog(CAER_LOG_CRITICAL, moduleData->moduleSubSystemString,
+	caerLog(CAER_LOG_DEBUG, moduleData->moduleSubSystemString,
 			"Initializing module ... %d", deviceType);
 
 	moduleData->moduleState = caerDeviceOpen(1, CAER_DEVICE_DYNAPSE, 0, 0,
