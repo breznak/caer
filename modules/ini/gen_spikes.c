a93031cc0ab561384e44518a908c981e9d0f41cb
federicohyo <federico.corradi@gmail.com>
2017-02-22 17:14:39 +0100

    fixed again gen spike default ns value according to POSIX can only be less than 1000 milion

--- modules/ini/gen_spikes.c
+++ modules/ini/gen_spikes.c
@@ -331,7 +331,7 @@ void spiketrainReg(void *spikeGenState) {
 		tim.tv_nsec = 1000000000L / atomic_load(&state->genSpikeState.stim_avr);
 	}
 	else {
-		tim.tv_nsec = 1000000000L;
+		tim.tv_nsec = 999999999L;
 	}
 
 	uint32_t value = atomic_load(&state->genSpikeState.core_d) | 0 << 16 | 0 << 17 | 1 << 13 |
@@ -399,7 +399,7 @@ void spiketrainPat(void *spikeGenState, uint32_t spikePattern[DYNAPSE_CONFIG_XCH
 		tim.tv_nsec = 1000000000L / atomic_load(&state->genSpikeState.stim_avr);
 	}
 	else {
-		tim.tv_nsec = 1000000000L; //1000000000L;
+		tim.tv_nsec = 999999999L; //1000000000L;
 	}
 
 	//generate chip command for stimulating
@@ -488,7 +488,7 @@ void spiketrainPatSingle(void *spikeGenState, uint32_t sourceAddress) {
 		tim.tv_nsec = 1000000000L / atomic_load(&state->genSpikeState.stim_avr);
 	}
 	else {
-		tim.tv_nsec = 1000000000L;
+		tim.tv_nsec = 999999999L;  // one sec
 	}
 
 	//generate chip command for stimulating
